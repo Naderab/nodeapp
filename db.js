6005425eed63b5656cb56e894a2d5e9666a7ad13
Naderab <abdellaouinader@gmail.com>
2023-12-21 09:59:53 +0100

    fix securiry mongouri

--- db.js
+++ db.js
@@ -1,18 +1,20 @@
 const mongoose = require('mongoose');
 
-const uri = process.env.NODE_ENV === 'test' ? process.env.MONGO_URI : 'mongodb://root:example@db:27017/';
+const uri = process.env.NODE_ENV === 'test' ? process.env.MONGO_URI : process.env.MONGO_URI_TEST;
 console.log(process.env.NODE_ENV);
 const connectDB = async () => {
     try {
-        await mongoose.connect(uri , 
-        {useNewUrlParser: true, 
-        useUnifiedTopology: true    });
-    console.log("MongoDB Connected")
+        await mongoose.connect(uri,
+            {
+                useNewUrlParser: true,
+                useUnifiedTopology: true
+            });
+        console.log("MongoDB Connected")
     }
     catch (error) {
         console.error(error.message);
         process.exit(1);
-    } 
+    }
 }
 
 module.exports = connectDB;
