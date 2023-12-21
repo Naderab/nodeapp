afd7a05c86b0b4c20c54b59021070e997d713dac
Naderab <abdellaouinader@gmail.com>
2023-12-21 14:05:22 +0100

    update mongo URI

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const uri = process.env.NODE_ENV === 'test' ? process.env.MONGO_URI_TEST : process.env.MONGO_URI;
+const uri = process.env.NODE_ENV === 'test' ? process.env.MONGO_URI : process.env.MONGO_URI_TEST;
 console.log(process.env.NODE_ENV);
 const connectDB = async () => {
     try {
